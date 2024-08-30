ce9ecfa00fa33c9e202ab1332eed9f5cb65cfa94
Yilu W <luwyl@yahoo.com>
2024-08-30 20:59:55 +1000

    change deck generation away from hardcoding

---
+++ src/deck.pl
@@ -0,0 +1,41 @@
+%% deck(?Deck)
+%
+%  Util predicate. 
+%  Deck is unified with a list of all 52 cards within a deck of a certain order. 
+deck(Deck) :- 
+    ranks(Ranks), 
+    suits(Suits), 
+    setof(Card, valid_card(Card, Ranks, Suits), Deck).
+
+%% ranks(?Ranks)
+%
+%  Util predicate. 
+%  Ranks is unified with a list of all 13 ranks within a deck. 
+%  For the purpose of this file, Ranks can only be of a certain order:
+%      [ace,jack,queen,king,2,3,4,5,6,7,8,9,10]
+%  If Ranks is passed in as a bound argument that violates this order, the 
+%  predicate returns false even if the bound argument can be extend to all 13 
+%  ranks of a deck in the real world. 
+ranks(Ranks) :- 
+    setof(Rank, between(2, 10, Rank), Rank_Nums),
+    append([ace,jack,queen,king], Rank_Nums, Ranks). 
+
+%% suits(?Suits)
+%
+%  Util predicate. 
+%  Suits is unified with a list of all 4 suits within a deck. 
+%  For the purpose of this file, Suits can only be of a certain order:
+%      [spades,hearts,clubs,diamonds]
+%  If Suits is passed in as a bound argument that violates this order, the 
+%  predicate returns false even if the bound argument can be extend to all 4 
+%  suits of a deck in the real world. 
+suits([spades,hearts,clubs,diamonds]). 
+
+
+%% valid_card(?Card, ?Ranks, ?Suits)
+%
+%  Util predicate. 
+%  Card is valid if its Rank is in Ranks, and Suit is in Suits. 
+valid_card(card(Rank, Suit), Ranks, Suits) :- 
+    member(Rank, Ranks), 
+    member(Suit, Suits). 
--- src/select_hand.pl
+++ src/select_hand.pl
@@ -1,3 +1,5 @@
+:- ensure_loaded(deck).
+
 %% select_hand(++Cards, -Hand, -Cribcards)
 select_hand(Cards, Hand, Cribcards) :- 
     possible_hand(Cards, [], Hands), 
@@ -14,28 +16,6 @@ possible_hand([Card1,Card2,Card3,Card4|Cards], Acc, Hands) :-
     possible_hand([Card1,Card2,Card4|Cards], Acc1, Acc2), 
     possible_hand([Card1,Card3,Card4|Cards], Acc2, Acc3), 
     possible_hand([Card2,Card3,Card4|Cards], Acc3, Hands). 
-
-deck(Deck) :- 
-    Deck = [ card(ace,hearts),card(2,hearts),card(3,hearts),card(4,hearts),
-             card(5,hearts),card(6,hearts),card(7,hearts),card(8,hearts),
-             card(9,hearts),card(10,hearts),
-             card(jack,hearts),card(queen,hearts),card(king,hearts),
-             
-             card(ace,spades),card(2,spades),card(3,spades),card(4,spades),
-             card(5,spades),card(6,spades),card(7,spades),card(8,spades),
-             card(9,spades),card(10,spades),
-             card(jack,spades),card(queen,spades),card(king,spades),
-             
-             card(ace,clubs),card(2,clubs),card(3,clubs),card(4,clubs),
-             card(5,clubs),card(6,clubs),card(7,clubs),card(8,clubs),
-             card(9,clubs),card(10,clubs),
-             card(jack,clubs),card(queen,clubs),card(king,clubs),
-             
-             card(ace,diamonds),card(2,diamonds),card(3,diamonds),card(4,diamonds),
-             card(5,diamonds),card(6,diamonds),card(7,diamonds),card(8,diamonds),
-             card(9,diamonds),card(10,diamonds),
-             card(jack,diamonds),card(queen,diamonds),card(king,diamonds)
-           ].
    
 %% set_difference(Set0, Set, Acc, Diff)
 set_difference([], _, Diff, Diff).
